bc58af08edf6f9b895eb0216a65c613929b1ad7d
Thilak2Raj7 <95743344+Thilak2Raj7@users.noreply.github.com>
2022-06-16 18:02:48 +0530

    Add files via upload

--- FirstTask/Vehicle.h
+++ FirstTask/Vehicle.h
@@ -2,77 +2,78 @@
 #define VEHICLE_H
 # include<iostream>
 using namespace std;
-class Vehicle{
+class Vehicle
+{
 
 private:
-string vehicle_type;
-string brand;
-string model;
-string color;
-int mileageOfVehicle;
-int price;
-int numberOfWheels;
+    string vehicle_type;
+    string brand;
+    string model;
+    string color;
+    int mileageOfVehicle;
+    int price;
+    int numberOfWheels;
 
 public:
-Vehicle(string brd,string mdl,string type,string clr,int mileage ,int amount)
-{
- vehicle_type=type;
- brand=brd;
- model=mdl;
- color=clr;
- mileageOfVehicle=mileage;
- price=amount;
-}
+    Vehicle(string brd,string mdl,string type,string clr,int mileage,int amount)
+    {
+        vehicle_type=type;
+        brand=brd;
+        model=mdl;
+        color=clr;
+        mileageOfVehicle=mileage;
+        price=amount;
+    }
 
 
 public :
-virtual int  getNumberOfWheels()=0;
+    virtual int  getNumberOfWheels()=0;
 
 
-string getBrand()
-{
-return brand;
-}
+    string getBrand()
+    {
+        return brand;
+    }
 
- string getColor()
-{
-return color;
-}
+    string getColor()
+    {
+        return color;
+    }
 
-int getPrice()
-{
-return price;
-}
- string getModel()
- {
- return model;
- }
-string getVehicleType()
-{
-return vehicle_type;
-}
+    int getPrice()
+    {
+        return price;
+    }
+    string getModel()
+    {
+        return model;
+    }
+    string getVehicleType()
+    {
+        return vehicle_type;
+    }
 
-bool operator >(Vehicle &p)
-{
-if(getPrice() > p.getPrice())
-{
-return true;
-}
+    bool operator >(Vehicle &p)
+    {
+        if(getPrice() > p.getPrice())
+        {
+            return true;
+        }
 
 
-return false;
-}
-friend ostream &operator << (ostream &out,Vehicle &vehicle)
-{
-out<<"The brand name of vehicle is  "<<vehicle.getBrand()<<endl;
-out<<"The model of vehicle is "<<vehicle.getModel()<<endl;
-out<<"The type of vehicle is  "<<vehicle.getVehicleType()<<endl;
-out<<"The colour of vehicle is  "<<vehicle.getColor()<<endl;
-out<<"The price of vehicle is  "<<vehicle.getPrice()<<endl;
-out <<"The number of wheels in car is   "<<vehicle.getNumberOfWheels()<<endl;
-out<<endl;
+        return false;
+    }
+    friend ostream &operator << (ostream &out,Vehicle &vehicle)
+    {
+        out<<"The brand name of vehicle is  "<<vehicle.getBrand()<<endl;
+        out<<"The model of vehicle is "<<vehicle.getModel()<<endl;
+        out<<"The type of vehicle is  "<<vehicle.getVehicleType()<<endl;
+        out<<"The colour of vehicle is  "<<vehicle.getColor()<<endl;
+        out<<"The price of vehicle is  "<<vehicle.getPrice()<<endl;
+        out <<"The number of wheels in car is   "<<vehicle.getNumberOfWheels()<<endl;
+        out<<endl;
 
-}
+    }
 
 };
 #endif
--- FirstTask/main.cpp
+++ FirstTask/main.cpp
@@ -3,29 +3,89 @@
 #include "Car.h"
 #include "Bike.h"
 using namespace std;
-
+  string vehicleName;
+    string model;
+    string vehicleType;
+    string color;
+    string mileage;
+    int price;
+    string type;
+    int numberOfPersons;
+  nameSpace
+  {
+  string getName()
+    {
+        cout<<"Enter the vehicle name"<<endl;
+        cin>>vehicleName;
+        return vehicleName;
+    }
+    string getModel()
+    {
+        cout<<"Enter the model name"<<endl;
+        cin>>model;
+        return model;
+    }
+    string getVehicleType()
+    {
+        cout<<"Enter the fuel type of vehicle"<<endl;
+        cin>>vehicleType;
+        return vehicleType;
+    }
+    string getColor()
+    {
+        cout<<"Enter the colour of vehicle"<<endl;
+        cin>>color;
+        return color;
+    }
+    int getMileage()
+    {
+        cout<<"Enter the mileage of vehicle"<<endl;
+        cin>>mileage;
+    }
+    int getPrice()
+    {
+        cout<<"Enter the price of vehicle"<<endl;
+        cin>>price;
+        return price;
+    }
+    string getType()
+    {
+        cout<<"Enter the type of vehicle"<<endl;
+        cin>>type;
+        return type;
+    }
+    int getNumberOfPerson()
+    {
+        cout<<"Enter the number of Person"<<endl;
+        cin>>numberOfPersons;
+        return numberOfPersons;
+    }
+    }
 int main()
 {
-Car veh1("Audi","R8","Diesel","Red",20,4600000,"SUV",7);
-cout<<veh1;
-
-Car veh2("Audi","A4","Diesel","Black",20,5000000,"SEDAN",4);
-cout<<veh2;
-
-Bike veh3("Pulsar","NS","Petrol","Black",35,100000,"ROADBIKE",152);
-cout<<veh3;
 
 
+    Car veh1(getName(),getModel(),getVehicleType(),getColor(),getMileage(),getPrice(),getType(),getNumberOfPerson());
+    cout<<veh1;
 
-if(veh1 > veh3)
-{
-cout<<"The vehicle 1 is more costlier than vehicle 2  "<<"true"<<endl;
-}
-else{
-cout<< "The vehicle 2 is more costlier than vehicle 1 "<<"false"<<endl;
-}
+    Car veh2(getName(),getModel(),getVehicleType(),getColor(),getMileage(),getPrice(),getType(),getNumberOfPerson());
+    cout<<veh2;
+
+    Bike veh3(  getName(),getModel(),getVehicleType(),getColor(),getMileage(),getPrice(),getType(),getNumberOfPerson());
+    cout<<veh3;
 
 
-}
+
+    if(veh1 > veh3)
+    {
+        cout<<"The vehicle 1 is more costlier than vehicle 2"   <<endl;
+    }
+    else
+    {
+        cout<< "The vehicle 2 is more costlier than vehicle 1 "<<endl;
+    }
+
+
+};
 
 
