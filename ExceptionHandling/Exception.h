cc11071e9eeda25995c0f8664010f29c8481305e
Thilak2Raj7 <95743344+Thilak2Raj7@users.noreply.github.com>
2022-06-21 12:24:21 +0530

    Add files via upload

--- ExceptionHandling/Exception.h
+++ ExceptionHandling/Exception.h
@@ -4,58 +4,59 @@
 #include <iostream>
 #include "MyException.h"
 using namespace std;
-class Exception{
-string msg;
+class Exception
+{
+    string msg;
 
 public:
- int getNumber(int a,int b)
- {
+    int getNumber(int a,int b)
+    {
 
- if(b==0)
- {
- msg="Divide by zero error";
- MyException myExcept(msg);
- throw myExcept;
- //throw myExcept;
- //throw "Divide by zero error";
-  }
+        if(b==0)
+        {
+            msg="Divide by zero error";
+            MyException myExcept(msg);
+            throw myExcept;
+//throw myExcept;
+//throw "Divide by zero error";
+        }
 
- else
- {
- return a/b;
+        else
+        {
+            return a/b;
 
- }
- }
+        }
+    }
 
- int getStringName(int* array,int index,int length )
-{
-if(length>index && index>=0)
-{
-return array[index];
-}
-else
-{
-msg="Array index is invalid";
-MyException except(msg);
-throw except;
-}
-}
+    int getStringName(int* array,int index,int length )
+    {
+        if(length>index && index>=0)
+        {
+            return array[index];
+        }
+        else
+        {
+            msg="Array index is invalid";
+            MyException except(msg);
+            throw except;
+        }
+    }
 
 
-char getStringIndex(string name,int index)
-{
-int length=name.length();
-if(length>index && index>=0)
-{
-return name[index];
-}
-else
-{
-msg="String index is invalid";
-MyException except(msg);
-throw except;
-}
-}
+    char getStringIndex(string name,int index)
+    {
+        int length=name.length();
+        if(length>index && index>=0)
+        {
+            return name[index];
+        }
+        else
+        {
+            msg="String index is invalid";
+            MyException except(msg);
+            throw except;
+        }
+    }
 
 
 
@@ -64,5 +65,3 @@ throw except;
 
 
 #endif // EXCEPTION1_H_INCLUDED
-
-
